f3cef48c86087f5d9a63602c3973af68e09615f4
Matt Goldstein <mattgoldstein18@gmail.com>
2023-08-17 10:44:57 -0700

    models added, starting view

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, redirect, render_template, flash
+from models import db, connect_db, User, Toddler, Word, ToddlerWord, WordArt
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///tinytalk'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "shhhhh"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+
+@app.route('/')
+def homepage():
+    
---
+++ models.py
@@ -0,0 +1,48 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    toddlers = db.relationship('Toddler', backref='parent', lazy=True)
+
+class Toddler(db.Model):
+    __tablename__ = 'toddlers'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    image_url = db.Column(db.String(255), nullable=True, default="https://media.istockphoto.com/id/1358260836/vector/toddler-sitting.jpg?s=612x612&w=0&k=20&c=Yo6Ct1RLLBdsdYM5eoyWOz8GY-2-7kuGn-6MWoSP68Y=") 
+    learned_words = db.relationship('ToddlerWord', backref='toddler', lazy=True)
+
+class Word(db.Model):
+    __tablename__ = 'words'
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(100), nullable=False, unique=True)
+    definition = db.Column(db.String(255), nullable=True)
+    learned_by = db.relationship('ToddlerWord', backref='word', lazy=True)
+    art = db.relationship('WordArt', backref='word', uselist=False)
+
+class ToddlerWord(db.Model):
+    __tablename__ = 'toddler_words'
+    id = db.Column(db.Integer, primary_key=True)
+    toddler_id = db.Column(db.Integer, db.ForeignKey('toddlers.id'), nullable=False)
+    word_id = db.Column(db.Integer, db.ForeignKey('words.id'), nullable=False)
+
+class WordArt(db.Model):
+    __tablename__ = 'word_arts'
+    id = db.Column(db.Integer, primary_key=True)
+    word_id = db.Column(db.Integer, db.ForeignKey('words.id'), nullable=False)
+    art_image_url = db.Column(db.String(255), nullable=False)
+
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
